a3e3f45cdace31892011ca6e5b2a6b2a95073f8a
LamiaeBoujaafar <lamiae20ber@gmail.com>
2021-04-30 21:15:03 +0200

    adding show-quiz v2.0

--- src/app/feature/student/show-quiz/show-quiz.component.ts
+++ src/app/feature/student/show-quiz/show-quiz.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {QuizService} from '../../../core/services/quiz-service/quiz.service';
-import {QuizModel} from '../../../core/models/quiz-model/quiz-model';
 import {NzModalService} from 'ng-zorro-antd/modal';
 import {ChapterService} from '../../../core/services/chapter-service/chapter.service';
 import {ChapterModel} from '../../../core/models/chapter-model/chapter-model';
 import {ChapterQuizModel} from '../../../core/models/chapter-model/chapter-quiz-model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-show-quiz',
@@ -14,7 +14,7 @@ import {ChapterQuizModel} from '../../../core/models/chapter-model/chapter-quiz-
 export class ShowQuizComponent implements OnInit {
 
   quizChapter: ChapterQuizModel[] = [];
-  selectedQuizChapter!:ChapterQuizModel;
+  selectedQuizChapter!: ChapterQuizModel;
   chapters: ChapterModel[] = [];
   currentQuiz = 0;
   answerSelected = false;
@@ -23,11 +23,13 @@ export class ShowQuizComponent implements OnInit {
   result: boolean = false;
   selectedChapter = false;
 
-  constructor(private quizService: QuizService, private chapterService: ChapterService, private modal: NzModalService) {
+  constructor(private quizService: QuizService, private chapterService: ChapterService, private modal: NzModalService, private route: Router) {
   }
+
   ngOnInit(): void {
     this.quizChapter = this.quizService.getQuizChapter();
   }
+
   onAnswer(correct: boolean) {
     this.answerSelected = true;
     setTimeout(() => {
@@ -35,7 +37,6 @@ export class ShowQuizComponent implements OnInit {
         this.currentQuiz++;
         this.answerSelected = false;
       }
-
     }, 1000);
     if (correct) {
       this.correctAnswers++;
@@ -44,23 +45,48 @@ export class ShowQuizComponent implements OnInit {
       console.log('incorrect' + correct);
     }
   }
+
   showResult() {
+
     if (this.correctAnswers >= 3) {
       this.modal.success({
         nzTitle: 'Congratulation!! YOU WIN',
-        nzContent: 'Correct answers : ' + this.correctAnswers + ' | ' + 'Incorrect answers : ' + this.incorrectAnswers
+        nzContent: 'Correct answers : ' + this.correctAnswers + ' | ' + 'Incorrect answers : ' + this.incorrectAnswers,
+        nzOkText: 'close',
+        nzOkType: 'primary',
+        nzOkDanger: false,
+        nzOnOk: () => {
+          this.initialize();
+        },
       });
     } else {
       this.modal.error({
         nzTitle: 'Sorry !! YOU LOSE',
-        nzContent: 'Correct answers : ' + this.correctAnswers + ' | ' + 'Incorrect answers : ' + this.incorrectAnswers
+        nzContent: 'Correct answers : ' + this.correctAnswers + ' | ' + 'Incorrect answers : ' + this.incorrectAnswers,
+        nzOkText: 'Yes',
+        nzOkType: 'primary',
+        nzOkDanger: true,
+        nzOnOk: () => {
+          this.initialize();
+        }
       });
     }
+  }
+
+  initialize() {
+    this.selectedChapter = false;
+    this.currentQuiz = 0;
+    this.answerSelected = false;
+    this.correctAnswers = 0;
+    this.incorrectAnswers = 0;
   }
 
   startQuiz(quizChapter: ChapterQuizModel) {
     this.selectedChapter = true;
     console.log(quizChapter);
     this.selectedQuizChapter = quizChapter;
+
+
+    //this.route.navigate(['dashboard/student/show-course']);
   }
 }
