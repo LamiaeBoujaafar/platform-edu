873fe2c580d7858540cac6dc567a4ddd3f06e526
oussamaFst <g.oussamaww2@gmail.com>
2021-05-02 17:13:32 +0000

    update add-student component

--- src/app/feature/institute/add-student/add-student.module.ts
+++ src/app/feature/institute/add-student/add-student.module.ts
@@ -6,18 +6,20 @@ import { AddStudentComponent } from './add-student.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzFormModule} from "ng-zorro-antd/form";
+import {NzInputModule} from "ng-zorro-antd/input";
 
 
 @NgModule({
   declarations: [
     AddStudentComponent
   ],
-  imports: [
-    CommonModule,
-    AddStudentRoutingModule,
-    ReactiveFormsModule,
-    NzButtonModule,
-    NzFormModule
-  ]
+    imports: [
+        CommonModule,
+        AddStudentRoutingModule,
+        ReactiveFormsModule,
+        NzButtonModule,
+        NzFormModule,
+        NzInputModule
+    ]
 })
 export class AddStudentModule { }
