b713e464d59590dcbbfc9367248c3e46835e113a
errajy <errajy@DESKTOP-59B7N37>
2021-05-06 22:58:26 +0000

    commit3

--- src/app/feature/prof/show-course-student-progress/show-course-student-progress.component.ts
+++ src/app/feature/prof/show-course-student-progress/show-course-student-progress.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CourseModel} from "../../../core/models/course/course-model/course-model";
+import {NzMessageService} from "ng-zorro-antd/message";
+import {StudentCourseModel} from "../../../core/models/course/student-course-model/student-course-model";
+import {StudentModel} from "../../../core/models/student-model/student-model";
 
 @Component({
   selector: 'app-show-course-student-progress',
@@ -7,26 +11,111 @@ import {Validators} from "@angular/forms";
   styleUrls: ['./show-course-student-progress.component.css']
 })
 export class ShowCourseStudentProgressComponent implements OnInit {
-  EtudiantCours = [];
-  cour:any;
+  showListEtudiantCours!:StudentCourseModel[];
+  validateForm!: FormGroup;
+  listOfCours:CourseModel[]=[
+    {
+      id : 0,
+      sections : [],
+      title : "java",
+      parcoursId : 0,
+      description:"cour java",
+      image: null
 
-  constructor() { }
+    },
+    {
+      id :1,
+      sections : [],
+      title : "php",
+      parcoursId : 0,
+      description:"cour php",
+      image: null
+
+    },
+  ];
+  Students:StudentModel[]=[
+    {
+      id:0,
+      dateOfBirth:"11:11/111",
+      email:"hdhaz@gmail.com",
+      firstName:"mohamed",
+      lastName:"errajy",
+      gender:"M",
+      login:"usename",
+      password:"gdhdsjhsdhcedh",
+      phone:"12223333322"
+
+    },
+    {
+      id:1,
+      dateOfBirth:"11:11/111",
+      email:"hdhaz@gmail.com",
+      firstName:"reda",
+      lastName:"errajy",
+      gender:"M",
+      login:"usename",
+      password:"gdhdsjhsdhcedh",
+      phone:"12223333322"
+
+    },
+  ]
+  ListEtudiantCours:StudentCourseModel[] = [
+    {
+      course:this.listOfCours[0],
+      id:0,
+      numberInvalidSection:23,
+      numberRemainingSection:10 ,
+      numberValidSection:40,
+  student:this.Students[0],
+     validParcours:true
+    },
+    {
+      course:this.listOfCours[0],
+      id:1,
+      numberInvalidSection:23,
+      numberRemainingSection:9 ,
+      numberValidSection:29,
+      student:this.Students[1],
+      validParcours:false
+    },
+    {
+      course:this.listOfCours[1],
+      id:1,
+      numberInvalidSection:100,
+      numberRemainingSection:9 ,
+      numberValidSection:200,
+      student:this.Students[1],
+      validParcours:false
+    },
+  ];
+  public SelectedCour!:CourseModel ;
+
+  constructor(private formBuilder: FormBuilder, private message: NzMessageService) { }
 
   ngOnInit(): void {
-    this.cour=[
-
-      {
-        titre : "php",
-        Descption: "php coure",
-        image: "imageX"
-      },
+    this.validateForm = this.formBuilder.group({
+      cours: [null, [Validators.required]],
+    });
+    // @ts-ignore
 
 
-    ];
-    this.EtudiantCours = [
-
-    ];
-    console.log(this.cour[0].titre);
   }
 
+  submitForm(data: any) {
+
+  }
+
+  changeCour(SelectData: any) {
+    this.SelectedCour=SelectData;
+
+      let listIndex :any []=[];
+    this.showListEtudiantCours = this.ListEtudiantCours.filter(course=>course.course== SelectData);
+
+      console.log(  this.showListEtudiantCours);
+
+  //  let index:number = this.ListEtudiantCours.findIndex(course=>course.course== SelectData);
+
+  console.log( this.showListEtudiantCours);
+
+  }
 }
--- src/app/feature/prof/show-course-student-progress/show-course-student-progress.module.ts
+++ src/app/feature/prof/show-course-student-progress/show-course-student-progress.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzMessageModule } from 'ng-zorro-antd/message';
-
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-
+import { NzDividerModule } from 'ng-zorro-antd/divider'
 import { ShowCourseStudentProgressRoutingModule } from './show-course-student-progress-routing.module';
 import { ShowCourseStudentProgressComponent } from './show-course-student-progress.component';
 
@@ -20,6 +19,7 @@ import { ShowCourseStudentProgressComponent } from './show-course-student-progre
     ShowCourseStudentProgressComponent
   ],
   imports: [
+    NzProgressModule,
     NzMessageModule,
     NzTableModule,
     NzCardModule,
