d2a777c8c2b9caea905eddaaa4fc784319058499
LamiaeBoujaafar <lamiae20ber@gmail.com>
2021-05-12 18:00:23 +0200

    add models22

--- src/app/feature/student/show-quiz/show-quiz.module.ts
+++ src/app/feature/student/show-quiz/show-quiz.module.ts
@@ -10,6 +10,7 @@ import {NzGridModule} from 'ng-zorro-antd/grid';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import {NzTypographyModule} from 'ng-zorro-antd/typography';
 
+
 @NgModule({
   declarations: [
     ShowQuizComponent
