8d7911e83ea90d7c6a079e4c5f619028fd8ce845
errajy <errajy@DESKTOP-59B7N37>
2021-06-01 15:32:01 +0000

    comitEnd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// @ts-ignore
 const routes: Routes = [
   { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
   { path: 'dashboard', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) },
