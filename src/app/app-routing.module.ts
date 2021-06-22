e6a06524bb36de850eb7e06b65bedc32a2ba0156
oussamaFst <g.oussamaww2@gmail.com>
2021-06-22 18:38:46 +0100

    push before deploy v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 // @ts-ignore
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+
   { path: 'dashboard', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) },
   { path: 'landing', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: '', loadChildren: () => import('./landing-page/first-page/first-page.module').then(m => m.FirstPageModule) },
   { path: 'navBar', loadChildren: () => import('./shared/nave-bar/nave-bar.module').then(m => m.NaveBarModule) },
   { path: 'first-page', loadChildren: () => import('./landing-page/first-page/first-page.module').then(m => m.FirstPageModule) },
   { path: 'registration-page', loadChildren: () => import('./landing-page/registration-page/registration-page.module').then(m => m.RegistrationPageModule) },
